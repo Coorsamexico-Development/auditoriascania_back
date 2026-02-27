7dd9faa7db156457534f5e2eefefefda53b1c25b
Tonatiuh Valladares <tonatiuhvalladares@gmail.com>
2026-02-27 09:10:51 -0600

    correcion time zone

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Forzar la zona horaria a la Ciudad de México globalmente para la aplicación o leerla de las variables de entorno
+process.env.TZ = process.env.TZ || 'America/Mexico_City';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
